2d6c549be3ae3544586b651a1ca7c8e915370682
Alex <alexseeley134@gmail.com>
2025-01-24 22:01:49 -0700

    Feat: Add services section with responsive design
    Built a services section to highlight app features
    with a responsive layout.
    
    Introduced semantic HTML elements (<section>,
    <figure>, <article>, <header>) for better
    structure and accessibility.
    
    Enhanced UX with a clear content hierarchy,
    visually separated sections
    
    Ensured consistency with Tailwind CSS styles and
    responsive design principles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <div className="flex flex-col items-stretch">
         <Hero />
-        <div id="services section" className="flex">
-          <div className=" flex-1 flex justify-center">
-            {/* These two divs define a full width container that use flexbox to justify the contents of the children to the center */}
-            <div className="w-full max-w-7xl text-left mx-3">
-              {/* This container is centered and has a max width of 7xl to keep it in line with the above hero image, but its background is full width */}{" "}
-              <h2>Discipleship App </h2>
-              <p>Description of discipleship app</p>
-            </div>
-          </div>
-        </div>
+        <Services />
       </div>
 
       <h1 className="text-3xl font-bold underline bg-primary">Hello world!</h1>
---
+++ src/components/Services.jsx
@@ -0,0 +1,46 @@
+import AppIllustration from "../assets/app-watch.svg";
+import { TabletSmartphone } from "lucide-react";
+
+export default function Services() {
+  return (
+    <section id="services section" className="flex mt-12">
+      <div className=" flex-1 flex justify-center">
+        {/* Wrapper for the services section with a max width */}
+
+        <div className="w-full max-w-7xl text-left mx-3">
+          {/* Content container with responsive layout */}{" "}
+          <div className="flex-col-reverse gap-12 md:flex-row flex items-center justify-center lg:gap-24 md:gap-12 px-6">
+            <figure className="w-80 md:w-1/2 lg:p-8  ">
+              <img src={AppIllustration} alt="" />
+            </figure>
+            <article className="md:w-1/2 w-auto">
+              <header className="flex items-center gap-6">
+                <TabletSmartphone className=" text-primaryMain lg:min-w-12 lg:min-h-12 min-w-10 min-h-10" />
+                <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold text-primaryMain">
+                  The App{" "}
+                </h2>
+              </header>
+              <hr className="my-3 w-full h-2 border-0 rounded-xl  bg-highlight" />
+              <h3 className="mb-2 ">
+                Discipleship Made Simple, Seamless, and Scalable
+              </h3>
+              <p
+                className=" lg:leading-normal md:leading-snug
+                  leading-relaxed"
+              >
+                Our app equips individuals with a personalized discipleship
+                journey through guided pathways for spiritual growth tailored to
+                every stage of faith. Leverage AI insights to deepen your
+                understanding of Scripture with powerful analysis and
+                personalized recommendations. Explore the full Bible using a
+                semantic search for topical studies and AI-generated insights.
+                Build community effortlessly with integrated group messaging
+                tools designed for teams and small groups.
+              </p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
