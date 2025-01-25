722d0b216b8277a37dedf2349118ce31a01da44b
Alex <alexseeley134@gmail.com>
2025-01-25 13:07:35 -0700

    Extract service block content into reusable comp

---
+++ src/components/ServiceBlock.jsx
@@ -0,0 +1,49 @@
+export default function ServiceBlock({
+  image,
+  imageAlt,
+  title,
+  subheading,
+  Icon,
+  paragraphContent,
+  imageOnLeft,
+}) {
+  return (
+    <div className="w-full max-w-7xl text-left mx-3 ">
+      {/* Content container with responsive layout */}{" "}
+      <div
+        className={`${
+          imageOnLeft ? "flex-col-reverse" : "flex-col"
+        } gap-12 md:flex-row flex items-center justify-center lg:gap-24 md:gap-12 px-6`}
+      >
+        {imageOnLeft && (
+          <figure className="w-80 md:w-1/2 lg:p-8  ">
+            <img src={image} alt={imageAlt} />
+          </figure>
+        )}
+
+        <article className="md:w-1/2 w-auto">
+          <header className="flex items-center gap-6">
+            <Icon className=" text-primaryMain lg:min-w-12 lg:min-h-12 min-w-10 min-h-10" />
+
+            <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold text-primaryMain">
+              {title}
+            </h2>
+          </header>
+          <hr className="my-3 w-full h-2 border-0 rounded-xl  bg-highlight" />
+          <h3 className="mb-2 ">{subheading}</h3>
+          <p
+            className=" lg:leading-normal md:leading-snug
+                    leading-relaxed"
+          >
+            {paragraphContent}
+          </p>
+        </article>
+        {!imageOnLeft && (
+          <figure className="w-80 md:w-1/2 lg:p-8  ">
+            <img src={image} alt={imageAlt} />
+          </figure>
+        )}
+      </div>
+    </div>
+  );
+}
