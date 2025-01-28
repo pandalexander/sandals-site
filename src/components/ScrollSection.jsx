e4935fdc38d231f49760a4597e77aa12b049b924
Alex Seeley <alexseeley134@gmail.com>
2025-01-28 13:04:30 -0700

    Redesign scroll section colors and hover

--- src/components/ScrollSection.jsx
+++ src/components/ScrollSection.jsx
@@ -6,22 +6,22 @@ export default function ScrollSection(props) {
     <div>
       <section
         id="scroll-wheel"
-        className="flex my-12 bg-primaryMain justify-center
+        className="flex my-12  justify-center
           "
       >
         {/* Wrapper for the services section with a max width */}
         <div className="w-full max-w-7xl  text-center mx-3">
           {/* Content container with responsive layout */}
-          <h2 className="text-5xl md:text-6xl my-10 text-baseLight">
+          <h2 className="text-5xl md:text-6xl my-10 text-primaryDark">
             Discipleship is not...
           </h2>
           <div className="overflow-hidden mt-6 flex xl:rounded-xl xl:mx-3">
-            <ul className="flex gap-10 text-primaryDark tracking-wider font-accent  animate-infinite-scroll">
+            <ul className="flex gap-10 text-accentLight tracking-wider font-accent  animate-infinite-scroll">
               {[...props.notDiscipleshipList, ...props.notDiscipleshipList].map(
                 (item, index) => {
                   return (
                     <li
-                      className="text-nowrap font-bold bg-baseLight rounded-xl px-4 py-2 hover:bg-white hover:cursor-default duration-300 transition-all hover:scale-95"
+                      className="text-nowrap font-bold bg-primaryMain rounded-xl px-4 py-2 hover:cursor-default duration-300 transition-all hover:scale-95"
                       key={index}
                     >
                       {item}
@@ -33,7 +33,7 @@ export default function ScrollSection(props) {
           </div>
 
           <div className="font-main mx-3 text-left text-accentLight text-lg my-10 space-y-6 md:flex md:gap-6 md:justify-center md:items-stretch md:space-y-0">
-            <div className="bg-baseLight text-primaryDark p-6 space-y-2 rounded-2xl hover:-translate-y-1 transition-all duration-300 hover:bg-white">
+            <div className="bg-primaryDark hover:rotate-2 hover:bg-opacity-90 shadow-xl p-6 space-y-2 rounded-2xl hover:-translate-y-1 transition-all duration-300 ">
               <CircleAlert className="text-amber-600 w-6 h-6" />{" "}
               <p className="">
                 While these efforts listed above can be valuable to believers
@@ -41,7 +41,7 @@ export default function ScrollSection(props) {
                 replacement for authentic biblical discipleship.
               </p>
             </div>
-            <div className="bg-baseLight text-primaryDark p-6 space-y-2 rounded-2xl hover:-translate-y-1 transition-all duration-300 hover:bg-white">
+            <div className="bg-accentMuted hover:-rotate-2 text-primaryDark hover:bg-opacity-90  p-6 space-y-2 rounded-2xl hover:-translate-y-1 transition-all duration-300">
               <TreeDeciduous className="text-primaryMain w-6 h-6" />{" "}
               <p className="">
                 Biblical Discipleship is bringing people to Christ and leading
