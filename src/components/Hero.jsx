1337e5026cbeea7b899fd19190595bbdc79087c2
Alex <alexseeley134@gmail.com>
2025-02-09 00:09:21 -0700

    Attempt to fix transition lagging on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,27 +19,41 @@ export default function Hero() {
 
   const root = useRef();
   const heroTriggerRef = useRef(null);
+  const heroImageRef = useRef(null);
 
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
       const el = heroTriggerRef.current;
+
       gsap.fromTo(
         ".box",
-        { y: 50, opacity: 0 },
+        { y: 50, autoAlpha: 0 },
         {
           y: 0,
           duration: 1,
-          opacity: 1,
+          autoAlpha: 1,
           stagger: 0.2,
           ease: "sine.out",
           force3D: true,
           scrollTrigger: { trigger: el },
         }
       );
+
+      gsap.fromTo(
+        ".image",
+        { y: -50, autoAlpha: 0 },
+        {
+          y: 0,
+          duration: 1,
+          autoAlpha: 1,
+          ease: "sine.out",
+          force3D: true,
+        }
+      );
     }, root);
 
     return () => ctx.revert();
-  }, []);
+  }, [heroIsLoaded]);
 
   return (
     <section
@@ -53,17 +67,21 @@ export default function Hero() {
         ref={heroTriggerRef}
         className=" flex-1 flex flex-col md:gap-8 gap-6"
       >
-        <h1 className="box font-bold text-baseDark text-4xl md:text-5xl lg:text-6xl !leading-snug text-balance">
+        <h1
+          className={`box font-bold text-baseDark text-4xl md:text-5xl lg:text-6xl !leading-snug text-balance`}
+        >
           Empower Your Church to Grow Together in{" "}
           <span className="bg-gradient-to-br from-primaryMain to-primaryDark bg-clip-text text-transparent">
             Discipleship
           </span>
         </h1>
-        <p className="box text-baseDark leading-loose text-lg md:text-xl text-balance">
+        <p
+          className={`box text-baseDark leading-loose text-lg md:text-xl text-balance`}
+        >
           Discover the next generation of ministry technology—where biblical
           discipleship meets the power of AI.
         </p>
-        <div className="box">
+        <div className={`box`}>
           <button
             className=" flex items-center justify-between md:mx-0 mx-auto px-6 bg-baseLight rounded-full p-2 text-baseDark font-bold text-lg max-w-64 hover:bg-primaryMain hover:text-baseLight hover:-translate-y-1 transition-all duration-200"
             aria-label="Download the app today"
@@ -75,12 +93,13 @@ export default function Hero() {
           </button>
         </div>
       </header>
-      <div className="flex-1 md:max-w-md max-w-96 box">
-        <img
-          src={heroUrl}
-          alt="Mockup of the app showcasing features"
-          className={`${heroIsLoaded ? "" : "hidden"} `}
-        />
+      <div
+        ref={heroImageRef}
+        className={`${
+          heroIsLoaded ? "image" : "opacity-0"
+        } flex-1 md:max-w-md max-w-96 `}
+      >
+        <img src={heroUrl} alt="Mockup of the app showcasing features" />
       </div>
     </section>
   );
