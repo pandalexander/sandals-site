0125511ddd2d7db07634bbcffa46fda49acb1601
Alex <alexseeley134@gmail.com>
2025-02-18 21:53:56 -0700

    Add animation to core principles section

--- src/components/CorePrinciples.jsx
+++ src/components/CorePrinciples.jsx
@@ -6,22 +6,28 @@ import ObeyWord from "../assets/ObeyWord.svg";
 import Pray from "../assets/Pray.svg";
 import ServeOthers from "../assets/ServeOthers.svg";
 import WorshipGod from "../assets/WorshipGod.svg";
-
 import PropTypes from "prop-types";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef, useLayoutEffect } from "react";
+import { useMediaQuery } from "@react-hook/media-query";
+gsap.registerPlugin(ScrollTrigger);
 
 function Principle({ Icon, title, paragraph }) {
   return (
-    <div
-      className="min-w-fit
-    p-6 flex flex-col gap-3 bg-baseLight to-primaryDark text-baseDark rounded-2xl transition-all duration-300 ease-in-out hover:bg-baseLight hover:cursor-default border-accentMuted border-2 hover:border-primaryMain hover:shadow-xl"
-    >
-      <div className="flex items-center justify-start gap-4">
-        <div className="h-12 w-12 flex justify-center items-center p-1">
-          <img src={Icon} alt="" className={`min-w-2 min-h-2`} />
+    <div className="animate-card">
+      <div
+        className=" min-w-fit
+      p-6 flex flex-col gap-3 bg-baseLight to-primaryDark text-baseDark rounded-2xl transition-transform duration-300 ease-in-out hover:bg-baseLight hover:cursor-default border-accentMuted border-2 hover:border-primaryMain hover:shadow-xl hover:-translate-y-1"
+      >
+        <div className="flex items-center justify-start gap-4">
+          <div className="h-12 w-12 flex justify-center items-center p-1">
+            <img src={Icon} alt="" className={`min-w-2 min-h-2`} />
+          </div>
+          <h3>{title}</h3>
         </div>
-        <h3>{title}</h3>
+        <p>{paragraph}</p>
       </div>
-      <p>{paragraph}</p>
     </div>
   );
 }
@@ -33,13 +39,69 @@ Principle.propTypes = {
 };
 
 const CorePrinciples = () => {
+  const isMobile = useMediaQuery("(max-width: 767px)");
+  const root = useRef();
+  const title = useRef();
+  const cardContainer = useRef();
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const cards = gsap.utils.toArray(".animate-card");
+
+      if (!isMobile) {
+        gsap.fromTo(
+          title.current,
+          { autoAlpha: 0, x: "-50vw" },
+          {
+            x: 0,
+            autoAlpha: 1,
+            ease: "power1.inOut",
+            duration: 1.5,
+            scrollTrigger: {
+              trigger: title.current, // Use el.current here as well
+              start: "bottom bottom",
+              scrub: false,
+              markers: false,
+            },
+          }
+        );
+
+        gsap.fromTo(
+          cards, // Target ALL boxes selected by '.animate-box'
+          { autoAlpha: 0, y: "100vh", rotate: -270 },
+          {
+            autoAlpha: 1,
+            y: 0,
+            stagger: 0.2,
+            rotate: 0,
+            ease: "power1.inOut",
+            duration: 2,
+            scrollTrigger: {
+              trigger: cardContainer.current, // Use el.current here as well
+              start: "top bottom",
+              scrub: false,
+              markers: false,
+            },
+          }
+        );
+      }
+    }, root);
+
+    return () => {
+      ctx.revert();
+    };
+  }, [isMobile]);
+
   return (
     <>
-      <div className="self-center my-12">
-        <h1 className="text-center my-6 text-primaryDark text-balance">
+      <div ref={root} className="self-center my-12">
+        <h1
+          ref={title}
+          className="text-center my-6 text-primaryDark text-balance"
+        >
           The Eight Facets of Spiritual Maturity
         </h1>
-        <div className="flex">
+        <div ref={cardContainer} className="flex">
           <div className="self-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-stretch  gap-6 flex-1  rounded-2xl my-6">
             <Principle
               Icon={WorshipGod}
